c8f15926c8df8ac5f461e6ef519239158606f601
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-07-23 11:18:46 +0300

    Added character images

--- fantastic-four-fan-evolution/src/components/CharacterCards.jsx
+++ fantastic-four-fan-evolution/src/components/CharacterCards.jsx
@@ -1,31 +1,36 @@
+import MrFantastic from "../assets/mr-fantastic.jpeg"
+import InvisibleWoman from "../assets/sue-storm.jpeg"
+import HumanTorch from "../assets/human-torch.jpeg"
+import TheThing from "../assets/the-thing.jpeg"
+
 const characters = [
     {
         name: "Mr. Fantastic",
         alias: "Reed Richards",
         power: "Super elasticity, genius intellect",
         color: "bg-blue-700",
-        image: "https://upload.wikimedia.org/wikipedia/en/3/33/Mr._Fantastic.png",
+        image: MrFantastic,
     },
     {
         name: "Invisible Woman",
         alias: "Sue Storm",
         power: "Invisibility and force fields",
         color: "bg-indigo-600",
-        image: "https://upload.wikimedia.org/wikipedia/en/6/60/Susan_Storm_Fantastic_Four.png",
+        image: InvisibleWoman,
     },
     {
         name: "Human Torch",
         alias: "Johnny Storm",
         power: "Pyrokinesis, flight",
         color: "bg-orange-500",
-        image: "https://upload.wikimedia.org/wikipedia/en/f/f8/Human_Torch_%28Johnny_Storm%29.png",
+        image: HumanTorch,
     },
     {
         name: "The Thing",
         alias: "Ben Grimm",
         power: "Superhuman strength, durability",
         color: "bg-yellow-600",
-        image: "https://upload.wikimedia.org/wikipedia/en/0/0c/Thing_Fantastic_Four.png",
+        image: TheThing,
     },
 ]
 
@@ -39,7 +44,7 @@ export default function CharacterCards() {
                         <img
                             src={char.image}
                             alt={char.name}
-                            className="w-full h-60 object-contain rounded-md mb-4 bg-black"
+                            className="w-full h-150 object-cover rounded-md mb-4 bg-black"
                         />
                         <h3 className="text-2xl font-semibold">{char.name}</h3>
                         <p className="text-sm italic mb-2">({char.alias})</p>
