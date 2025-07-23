e3fcc179f7da6b3c6b876013628df616f386fff6
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-07-23 11:05:49 +0300

    - Cleared App - Added countdown to App

--- fantastic-four-fan-evolution/src/App.jsx
+++ fantastic-four-fan-evolution/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Countdown from "./components/Countdown.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen bg-ff-dark text-ff-white">
+          <h1 className="text-4xl text-center font-bold py-8 text-ff-blue">
+              Fantastic Four: Fan Evolution
+          </h1>
+          <span className="text-ff-flame hover:animate-pulse">Human Torch Mode</span>
+
+          {/* Other components */}
+          <div className="flex justify-center">
+              <Countdown />
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
