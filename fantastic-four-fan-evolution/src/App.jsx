2e3cc69f68219a87c11a6b63948d547167328f28
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-07-23 12:23:59 +0300

    Updated App

--- fantastic-four-fan-evolution/src/App.jsx
+++ fantastic-four-fan-evolution/src/App.jsx
@@ -1,20 +1,23 @@
 import './App.css'
 import Countdown from "./components/Countdown.jsx";
 import CharacterCards from "./components/CharacterCards.jsx";
+import MarvelF4 from './assets/images/marvel-first-steps.jpeg'
 
 function App() {
   return (
       <div className="min-h-screen bg-ff-dark text-ff-white">
-          <h1 className="text-4xl text-center font-bold py-8 text-ff-blue">
-              Fantastic Four: Fan Evolution
+          <h1 className="text-4xl text-center font-bold py-8 text-blue-400">
+              Fantastic ④ Countdown
           </h1>
+          <div>
+              <img src={MarvelF4} alt="Marvel Fantastic Four" className={ "w-full h-100 object-cover mx-auto rounded-lg"}/>
+          </div>
+          {/*
           <span className="text-ff-flame hover:animate-pulse">Human Torch Mode</span>
-
-          {/* Other components */}
+          */}
           <div className="flex justify-center">
               <Countdown />
           </div>
-
           <CharacterCards />
       </div>
   )
