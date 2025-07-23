076a60d87f6648ae8262297c93adcc86c8a9deef
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-07-23 11:07:47 +0300

    - Defined f4 characters in CharacterCards.jsx

---
+++ fantastic-four-fan-evolution/src/components/CharacterCards.jsx
@@ -0,0 +1,30 @@
+const characters = [
+    {
+        name: "Mr. Fantastic",
+        alias: "Reed Richards",
+        power: "Super elasticity, genius intellect",
+        color: "bg-blue-700",
+        image: "https://upload.wikimedia.org/wikipedia/en/3/33/Mr._Fantastic.png",
+    },
+    {
+        name: "Invisible Woman",
+        alias: "Sue Storm",
+        power: "Invisibility and force fields",
+        color: "bg-indigo-600",
+        image: "https://upload.wikimedia.org/wikipedia/en/6/60/Susan_Storm_Fantastic_Four.png",
+    },
+    {
+        name: "Human Torch",
+        alias: "Johnny Storm",
+        power: "Pyrokinesis, flight",
+        color: "bg-orange-500",
+        image: "https://upload.wikimedia.org/wikipedia/en/f/f8/Human_Torch_%28Johnny_Storm%29.png",
+    },
+    {
+        name: "The Thing",
+        alias: "Ben Grimm",
+        power: "Superhuman strength, durability",
+        color: "bg-yellow-600",
+        image: "https://upload.wikimedia.org/wikipedia/en/0/0c/Thing_Fantastic_Four.png",
+    },
+]
